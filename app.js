b5c6be6a4c35346cc00267aa8de5962161b72aaa
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-04-24 19:07:25 +0100

    changes and hides all the text and buttons on the sign up page when you click to sign-in

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-document.querySelector('#join').addEventListener('click', function() {
+const spanbtn = document.getElementById("spanbtn");
 
-    document.querySelector('#signscroll').scrollIntoView({
-      behavior: 'smooth'
-    });
-  
+//Span tag on the signup page
+spanbtn.addEventListener("click", spanSign);
+function spanSign() {
+  document.getElementById("name").style.display = "none";
+  document.getElementById("signupBtn").style.setProperty("background-color", "#33A5FF");
+  document.getElementById("signupBtn").textContent="Sign In";
+  document.getElementById("googletext").textContent="Sign In with Google";
+  document.getElementById("signedin").style.display = "none";
+  document.getElementById("signedup2").style.display = "block";
+  document.getElementById("welcome").textContent = "Hello Again.";
+  document.getElementById("signtoruche").textContent = "Sign in to Ruche";
+  document.getElementById("email").placeholder = "Email or username";
+  document.getElementById("password").placeholder = "Password";
+  document.getElementById("createpassword").style.display = "none";
+  document.getElementById("youremail").style.display = "none";
+  document.getElementById("youremail").style.display = "none";
+}
+
+
+document.querySelector("#join").addEventListener("click", function () {
+  document.querySelector("#signscroll").scrollIntoView({
+    behavior: "smooth",
   });
+});
